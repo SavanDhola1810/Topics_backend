7d1f0f09274aac8e7685815e056347f5cb1bbedf
SavanDhola1810 <123728753+SavanDhola1810@users.noreply.github.com>
2023-08-19 10:44:04 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const mongoURI=process.env.URL;
 
 const connectToMongo=()=>{
-    mongoose.connect(mongoURI,, { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 }
 
 module.exports=connectToMongo;
